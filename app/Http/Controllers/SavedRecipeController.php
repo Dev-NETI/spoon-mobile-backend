f76688d71e9c490ff0b28428ef70e76b2a61ec75
MysticMaccc <cosmicsher96@gmail.com>
2024-09-02 13:29:24 +0800

    added recipeId to show method of SavedRecipeController

--- app/Http/Controllers/SavedRecipeController.php
+++ app/Http/Controllers/SavedRecipeController.php
@@ -28,8 +28,9 @@ class SavedRecipeController extends Controller
         return response()->json($savedRecipes);
     }
 
-    public function show($user){
-         $savedRecipes = SavedRecipe::from('saved_recipes as a')
+    public function show($user)
+    {
+        $savedRecipes = SavedRecipe::from('saved_recipes as a')
             ->join('recipes as b', 'a.recipe_id', '=', 'b.id')
             ->join('recipe_origins as c', 'b.recipe_origin_id', '=', 'c.id')
             ->where('a.is_active', 1)
@@ -38,6 +39,7 @@ class SavedRecipeController extends Controller
             ->select([
                 'a.modified_by',
                 'b.name',
+                'b.id',
                 'b.image_path AS recipe_img',
                 'c.name AS origin_name',
                 'c.image_path',
